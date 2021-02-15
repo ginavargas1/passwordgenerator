4849871426eb06374bd46dd51734d9cda72f29e3
Gina Vargas <gina.vargas89@outlook.com>
2021-02-15 15:28:37 -0800

    solved

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Adding symbols for function  
 const symbolsSet = "!@#$%^&*(){}[]=<>/,.";
-const generateButton = document.getElementById('generateBtn')
+const generateButton = document.getElementById('generate')
 generateButton.addEventListener('click', writePassword)
 
 
@@ -17,85 +17,85 @@ function writePassword() {
 }
 
 // Adding prompts 
-  function generatePassword () {
-    var passwordLength = prompt ('How many characters would you like your password to be? Password must be a minimum of 8 and maximum of 128.');
-    var numbers = confirm ('Would you like numbers in your password?');
-    var lowerCases = confirm ('Would you like lowercases in your password?');
-    var upperCases = confirm ('Would you like uppercases in your password?');
-    var symbols = confirm ('Would you like to add special symbols?')
+function generatePassword() {
+  var passwordLength = prompt('How many characters would you like your password to be? Password must be a minimum of 8 and maximum of 128.');
+  var numbers = confirm('Would you like numbers in your password?');
+  var lowerCases = confirm('Would you like lowercases in your password?');
+  var upperCases = confirm('Would you like uppercases in your password?');
+  var symbols = confirm('Would you like to add special symbols?')
 
-    // adding a minimum amount of numbers, lowerCases, upperCases & symbols
-    var minimumCount = 0;
+  // adding a minimum amount of numbers, lowerCases, upperCases & symbols
+  var minimumCount = 0;
 
-    var minimumNumbers = "";
-    var minimumLowerCases = "";
-    var minimumUpperCases = "";
-    var minimumSymbolsSet ="";
+  var minimumNumbers = "";
+  var minimumLowerCases = "";
+  var minimumUpperCases = "";
+  var minimumSymbolsSet = "";
 
-    window.addEventListener('load', function() {
-      generateNewPassword();
-    });
+  window.addEventListener('load', function () {
+    generateNewPassword();
+  });
 
-    // adding functions
-    var functionArray = [
-      function getNumbers () {
-        return String.fromCharCode(Math.floor(Math.random()*10+48));
-      },
+  // adding objects and then value
+  var functionObject = {
+    getNumbers: function () {
+      return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+    },
 
-      function getLowerCases () {
-        return String.fromCharCode(Math.floor(Math.random()* 26 + 97));
-      },
+    getLowerCases: function () {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+    },
 
-      function getUpperCases () {
-        return String.fromCharCode(Math.floor(Math.random()* 26 + 65));
-      },
+    getUpperCases: function () {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+    },
 
-      function getSymbolsSet (){
-          return symbolsSet(Math.floor(Math.random()* symbolsSet.length));
-      }
-    ];
-
-    if (numbers === true) {
-      minimumNumbers = functionArray.getNumbers ();
-      minimumCount ++;
+    getSymbolsSet: function () {
+      return symbolsSet[Math.floor(Math.random() * symbolsSet.length)];
     }
+  };
 
-    if (lowerCases === true) {
-      minimumLowerCases = functionArray.getLowerCases ();
-      minimumCount ++;
-    }
+  if (numbers === true) {
+    minimumNumbers = functionObject.getNumbers();
+    minimumCount++;
+  }
 
-    if (upperCases === true) {
-      minimumUpperCases = functionArray.getUpperCases();
-      minimumCount++;
-  
-    }
-  
-    if (special === true) {
-      minimumSymbolsSet = functionArray.getSymbolsSet();
-      minimumCount++;
-  
-    }
+  if (lowerCases === true) {
+    minimumLowerCases = functionObject.getLowerCases();
+    minimumCount++;
+  }
 
-    var randomPasswordGenerated = "";
-
-    // for loop
-    for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
-      var randomNumberPicked = Math.floor(Math.random() * 4);
-  
-      randomPasswordGenerated += randomNumberPicked;
-  
-    }
-  
-    randomPasswordGenerated += minimumNumbers;
-    randomPasswordGenerated += minimumLowerCases;
-    randomPasswordGenerated += minimumUpperCases;
-    randomPasswordGenerated += minimumSymbolsSet;
-  
-  
-    return randomPasswordGenerated;
+  if (upperCases === true) {
+    minimumUpperCases = functionObject.getUpperCases();
+    minimumCount++;
 
   }
+
+  if (symbols === true) {
+    minimumSymbolsSet = functionObject.getSymbolsSet();
+    minimumCount++;
+
+  }
+
+  var randomPasswordGenerated = "";
+
+  // for loop
+  for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
+    var randomNumberPicked = Math.floor(Math.random() * 4);
+
+    randomPasswordGenerated += randomNumberPicked;
+
+  }
+
+  randomPasswordGenerated += minimumNumbers;
+  randomPasswordGenerated += minimumLowerCases;
+  randomPasswordGenerated += minimumUpperCases;
+  randomPasswordGenerated += minimumSymbolsSet;
+
+
+  return randomPasswordGenerated;
+
+}
 
 
 
