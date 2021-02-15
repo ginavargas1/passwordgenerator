22bd9b1671186d7454975ea05d0597d3810d810a
Gina Vargas <gina.vargas89@outlook.com>
2021-02-14 21:51:13 -0800

    adding new variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  var length = 8;
-  var setCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  var retVal = "";
-  for (var i = 0, n = setCharacters.length; i < length; i++) {
-      retVal += setCharacters.charAt(Math.floor(Math.random() * n));
-  }
-  return retVal;
-}
 
- generatePassword();
+
+
+
+
+
+
+
+// Code will generate password by only clicking the 'Generate Password' button
+// function generatePassword() {
+//   var length = 8;
+//   var setCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//   var password = "";
+//   for (i = 0, n = setCharacters.length; i < length; i++) {
+//       password += setCharacters.charAt(Math.floor(Math.random() * n));
+
+    
+//   }
+//   return password;
+// }
+
+//  generatePassword();
+
+
 
 // Write password to the #password input
 function writePassword() {
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # passwordgenerator
+
+When you click the button 'Generate Password' 
+You should be presented with 8 special characters in total 
+The set of characters will diferentiate in order 
